3ae9918b838f91c0ea0c761d82af3c7695a081b6
Isamu Mogi <isamu@leafytree.jp>
2017-07-23 05:33:19 +0900

    Optimization 1

--- warp/Network/Wai/Handler/Warp/IO.hs
+++ warp/Network/Wai/Handler/Warp/IO.hs
@@ -3,27 +3,39 @@
 
 module Network.Wai.Handler.Warp.IO where
 
-import Data.ByteString (ByteString)
+import Data.ByteString as BS (ByteString, length)
 import Data.ByteString.Builder (Builder)
 import Data.ByteString.Builder.Extra (runBuilder, Next(Done, More, Chunk))
+import Foreign.Ptr (plusPtr)
 import Network.Wai.Handler.Warp.Buffer
 import Network.Wai.Handler.Warp.Types
 
 toBufIOWith :: Buffer -> BufSize -> (ByteString -> IO ()) -> Builder -> IO ()
-toBufIOWith buf !size io builder = loop firstWriter
+toBufIOWith buf !size io builder = loop firstWriter 0
   where
     firstWriter = runBuilder builder
     runIO len = bufferIO buf len io
-    loop writer = do
-        (len, signal) <- writer buf size
+    loop writer writtenSize = do
+        (len, signal) <- writer (buf `plusPtr` writtenSize) $ size - writtenSize
         case signal of
-             Done -> runIO len
+             Done -> runIO $ writtenSize + len
              More minSize next
                | size < minSize -> error "toBufIOWith: BufferFull: minSize"
+               | size - writtenSize >= minSize -> loop next $ writtenSize + len
                | otherwise      -> do
-                   runIO len
-                   loop next
-             Chunk bs next -> do
-                 runIO len
-                 io bs
-                 loop next
+                   runIO $ writtenSize + len
+                   loop next 0
+             Chunk bs next
+               | size - writtenSize - len >= bsLen -> do
+                   _ <- copy (buf `plusPtr` (writtenSize + len)) bs
+                   loop next $ writtenSize + len + bsLen
+               | size >= bsLen  -> do
+                   runIO $ writtenSize + len
+                   _ <- copy buf bs
+                   loop next bsLen
+               | otherwise      -> do
+                   runIO $ writtenSize + len
+                   io bs
+                   loop next 0
+               where
+                 bsLen = BS.length bs
